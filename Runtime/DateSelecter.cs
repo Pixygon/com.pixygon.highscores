580131caeb098f6f1c02f7174a8f2800323dfb89
gentlemanSoup <anders.horne@gmail.com>
2023-09-12 20:31:46 +0200

    Added dates and shit

---
+++ Runtime/DateSelecter.cs
@@ -0,0 +1,48 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Pixygon.Highscores {
+    public class DateSelecter : MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI _dayText;
+        [SerializeField] private TextMeshProUGUI _monthText;
+        [SerializeField] private TextMeshProUGUI _yearText;
+        
+        [SerializeField] private int _startOffset = 0;
+        
+        private DateTime time;
+
+        public Action<string> OnDateChange;
+
+        private void Start() {
+            time = DateTime.Today;
+            time.Subtract(new TimeSpan(_startOffset, 0, 0, 0));
+            SetDate();
+        }
+        public void OnChangeDay(bool up) {
+            time.AddDays(up ? 1 : -1);
+            SetDate();
+        }
+
+        public void OnChangeMonth(bool up) {
+            time.AddMonths(up ? 1 : -1);
+            SetDate();
+        }
+
+        public void OnChangeYear(bool up) {
+            time.AddYears(up ? 1 : -1);
+            SetDate();
+        }
+
+        private void SetDate() {
+            if(time >= DateTime.Today)
+                time = DateTime.Today;
+            if (time < new DateTime(2023, 8, 23)) ;
+                time = new DateTime(2023, 8, 23);
+            _dayText.text = time.Day.ToString();
+            _monthText.text = time.Month.ToString();
+            _yearText.text = time.Year.ToString();
+            OnDateChange?.Invoke($"{time.Year}-{time.Month}-{time.Day}");
+        }
+    }
+}
--- Runtime/HighscoreTable.cs
+++ Runtime/HighscoreTable.cs
@@ -9,25 +9,53 @@ namespace Pixygon.Highscores {
         [SerializeField] private ScoreObject _highscoreObjectPrefab;
         [SerializeField] private Transform _highscoreParent;
         [SerializeField] private TextMeshProUGUI _scoreTypeText;
+        [SerializeField] private TextMeshProUGUI _versionText;
+        [SerializeField] private DateSelecter _fromDateSelector;
+        [SerializeField] private DateSelecter _toDateSelector;
 
         private List<ScoreObject> _scores;
         private string _searchType = "score";
-        public string Version { get; set; }
         public string Game { get; set; }
+        public string[] Versions { get; set; }
+        public int CurrentVersion { get; set; }
 
         public string FromDate { get; set; } = "2023-09-10";
         public string ToDate { get; set; } = "2023-09-12";
 
+
         private void Awake() {
             _scoreTypeText.text = _searchType;
+            CurrentVersion = Versions.Length - 1;
+            _versionText.text = "v" + Versions[CurrentVersion];
+            _fromDateSelector.OnDateChange = s => { FromDate = s; GetHighscores(); };
+            _toDateSelector.OnDateChange = s => { ToDate = s; GetHighscores(); };
         }
+        
         public async void GetHighscores() {
             PopulateBoard(JsonUtility.FromJson<Highscores>(await PixygonApi.GetHighScores(
-                Game, _searchType, Version, FromDate, ToDate)));
+                Game, _searchType, Versions[CurrentVersion], FromDate, ToDate)));
         }
 
         public static void PostHighscore(string game, string user, int score, int kills, float time, string detail, string version = "1.0.0", int multiplierPercent = 100) {
             PixygonApi.PostHighScore(JsonUtility.ToJson(new HighScore(game, user, "", score, kills, time, detail, version, multiplierPercent)));
+        }
+
+        public void OnNextVersion(int i) {
+            if (i == -1) {
+                if (CurrentVersion == 0)
+                    CurrentVersion = Versions.Length - 1;
+                else
+                    CurrentVersion -= 1;
+            }
+            else {
+                if (CurrentVersion == Versions.Length - 1)
+                    CurrentVersion = 0;
+                else
+                    CurrentVersion += 1;
+            }
+
+            _versionText.text = "v" + Versions[CurrentVersion];
+            GetHighscores();
         }
 
         public void SetSearchType(int i) {
